6e01679f10ea4f17bba55d70c031390f64254192
Aof <aof@freshconsulting.com>
2016-12-26 02:14:34 +0700

    updated config

--- app/scripts/directives/dir-chartjs.js
+++ app/scripts/directives/dir-chartjs.js
@@ -10,7 +10,7 @@ angular.module('dirChartjsApp')
     .directive('dirChartjs', [function () {
         return {
             template: '<div class="dir-chartjs">' +
-                '<lable for="cumulative-option">' +
+                '<lable for="cumulative-option" ng-show="isRendered">' +
                     '<input id="cumulative-option" type="checkbox" ng-model="isCumulative" /> Cumulative' +
                 '</lable>' +
                 '<canvas class="dir-chartjs__canvas"></canvas>' +
@@ -49,14 +49,29 @@ angular.module('dirChartjsApp')
                     }
                 };
 
-                // set defaults point
-                Chart.defaults.global.elements.point.radius = 3;
-                Chart.defaults.global.elements.point.hitRadius = 3;
-                Chart.defaults.global.elements.point.hoverRadius = 4;
+                // set defaults point configuration.
+                Chart.defaults.global.elements.point = {
+                    radius: 3,
+                    hitRadius: 3,
+                    hoverRadius: 4
+                };
+
+                // set defaults label configuration.
+                Chart.defaults.global.legend = {
+                    position: 'top',
+                    labels : {
+                        fontSize: 12,
+                        fontStyle: "normal",
+                        fontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+                    }
+                };
 
                 // set default value
+                scope.data = scope.data || [];
                 scope.options = (scope.options && angular.merge(scope.options, options)) || options;
                 scope.isCumulative = false;
+                scope.isRendered = false;
+                scope.chartInstance = null;
                 scope.chartType = scope.chartType || 'line';
                 scope.canvasWidth = scope.canvasWidth || "100%";
                 scope.canvasHeight = scope.canvasHeight || 100;
@@ -66,66 +81,75 @@ angular.module('dirChartjsApp')
                 // set canvas height
                 canvasElem[0].height = scope.canvasHeight;
 
-                scope.data = scope.data || {
-                    // label on axis
-                    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-                    datasets: [{
-                        // label show when hover
-                        label: 'apples',
-                        // data
-                        data: [12, 19, 3, 17, 6, 3, 7],
-                        borderColor: "rgba(75,192,192,1)",
-                        backgroundColor: "rgba(153,255,51,0.6)"
-                    }, {
-                        // label show when hover
-                        label: 'oranges',
-                        data: [2, 29, 5, 5, 2, 3, 10],
-                        backgroundColor: "rgba(255,153,0,0.6)"
-                    }]
-                };
-
-                // create chart
-                var chartInstance = new Chart(ctx, {
-                    type: scope.chartType,
-                    data: scope.data,
-                    options: options
-                });
-
                 // Observe data
                 scope.$watch('data', function observeData(newVal, oldVal) {
-                    if(!angular.isArray(newVal)) { // for object
+                    if(newVal && !angular.isArray(newVal)) { // for object
                         transformDataForObject(newVal);
-                    }else { // for array
-                        transformDataForArray(newVal);
                     }
                 });
 
                 // Observe cumulative option
                 scope.$watch('isCumulative', function checkCumulative(newVal, oldVal) {
-                    chartInstance.options.scales.yAxes[0].stacked = newVal;
-                    chartInstance.update();
+                    if(scope.chartInstance) {
+                        scope.chartInstance.options.scales.yAxes[0].stacked = newVal;
+                        scope.chartInstance.update();
+                    }
                 });
 
                 // Destroy chart instance
                 scope.$on('$destroy', function destroy() {
-                    chartInstance.destroy();
+                    scope.chartInstance.destroy();
                 });
 
                 // Observe on window resize.
                 $(window).on('resize', function resizeWindow() {
-                    chartInstance.resize();
+                    if(scope.chartInstance) {
+                        scope.chartInstance.resize();
+                    }
                 });
 
                 // transform data for object
-                function transformDataForObject(data) {
-
+                function transformDataForObject(dataObj) {
+                    var data = {
+                        // label on axis
+                        labels: [],
+                        // Example.
+                        // datasets: [{
+                        //     // label show when hover
+                        //     label: 'apples',
+                        //     // data
+                        //     data: [12, 19, 3, 17, 6, 3, 7],
+                        //     borderColor: "rgba(75,192,192,1)",
+                        //     backgroundColor: "rgba(153,255,51,0.6)"
+                        // }, {
+                        //     // label show when hover
+                        //     label: 'oranges',
+                        //     data: [2, 29, 5, 5, 2, 3, 10],
+                        //     backgroundColor: "rgba(255,153,0,0.6)"
+                        // }],
+                        datasets: []
+                    };
+                    angular.forEach(dataObj, function transformObject(val, key) {
+                        // data.labels.push(); // #TODO Tooltip data
+                        data.datasets.push({
+                            label: key,
+                            data: val.data,
+                            borderColor: val.color, // #TODO darken color
+                            backgroundColor: val.color // #TODO lighten color
+                        });
+                    });
+                    renderChart(data);
                 }
 
-                // transform data for array
-                function transformDataForArray(data) {
-                    if(!!data.length) {
-
-                    }
+                // render chart
+                function renderChart(data) {
+                    // create chart
+                    scope.chartInstance = new Chart(ctx, {
+                        type: scope.chartType,
+                        data: data,
+                        options: scope.options
+                    });
+                    scope.isRendered = true;
                 }
             }
         };
